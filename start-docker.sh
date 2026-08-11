ddcfd270b45107fd96db74d2c63582ab345eca61
Jacob <jacob.folch@gmail.com>
2026-08-11 20:42:03 +0200

    fix:docker:wrong volume target for archive

--- start-docker.sh
+++ start-docker.sh
@@ -3,5 +3,5 @@ docker build -t ssh-forum-v2 . || exit 1
 docker run -d -p 6666:2222 \
   --name "analogcity-2026" \
   -v "$(pwd)/boards:/usr/src/app/boards:rw" \
-  -v "$(pwd)/boards:/usr/src/app/archive:rw" \
+  -v "$(pwd)/archive:/usr/src/app/archive:rw" \
   ssh-forum-v2
